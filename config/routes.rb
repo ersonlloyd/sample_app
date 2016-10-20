56336b273220eb90e1e767e568ba3d1e1e75352e
@ersonlloyd <erson@proudcloud.io>
2016-10-20 14:51:57 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  get '/home', to: 'static_pages#home'
 
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/contacts'
+  get '/about', to: 'static_pages#about'
+
+  get '/contacts', to: 'static_pages#contacts'
+
+  get '/signup', to: 'users#new'
 
   root 'static_pages#home'
 
